5414b923383f50e3c120a256a568a79a70f523fb
Hector Haro H <hector8.haro@gmail.com>
2022-08-06 17:05:16 -0500

    add more javascript

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
+async function getTrendingMoviesPreview() {
+    const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await response.json();
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
 
-console.log('API URL = https://luhiulhiusf.com/ahiuhiudaiu?api_jey=' + API_KEY)
+        const movieImage = document.createElement('img');
+        movieImage.classList.add('movie-img');
+        movieImage.setAttribute('alt', movie.title);
+        movieImage.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+        movieContainer.appendChild(movieImage);
+
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+
+    });
+}
+
+getTrendingMoviesPreview();
