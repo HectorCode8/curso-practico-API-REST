0ae6f5cb3b97579eb43387add688ec1c61df39e6
Hector Haro H <hector8.haro@gmail.com>
2022-08-08 10:21:11 -0500

    add new changes

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 async function getTrendingMoviesPreview() {
-    const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await response.json();
-    const movies = data.results;
-    movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie-container');
+  const response = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
+  );
+  const data = await response.json();
+  const movies = data.results;
+  movies.forEach((movie) => {
+    const trendingPreviewMoviesContainer = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
 
-        const movieImage = document.createElement('img');
-        movieImage.classList.add('movie-img');
-        movieImage.setAttribute('alt', movie.title);
-        movieImage.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
-        movieContainer.appendChild(movieImage);
+    const movieImage = document.createElement("img");
+    movieImage.classList.add("movie-img");
+    movieImage.setAttribute("alt", movie.title);
+    movieImage.setAttribute(
+      "src",
+      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+    );
+    movieContainer.appendChild(movieImage);
 
-        trendingPreviewMoviesContainer.appendChild(movieContainer);
-
-    });
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
 }
 
 getTrendingMoviesPreview();
