e93ca0baacac5849a9f53fe7e44f60dc38577130
Hector Haro H <hector8.haro@gmail.com>
2022-08-12 16:42:19 -0500

    finish all categories

--- src/navigation.js
+++ src/navigation.js
@@ -1,3 +1,15 @@
+searchFormBtn.addEventListener("click", () => {
+  location.hash = "#search=";
+});
+
+trendingBtn.addEventListener("click", () => {
+  location.hash = "#trends";
+});
+
+arrowBtn.addEventListener("click", () => {
+  location.hash = "#home";
+});
+
 window.addEventListener("DOMContentLoaded", navigator, false);
 window.addEventListener("hashchange", navigator, false);
 
@@ -21,16 +33,17 @@ function homePage() {
   console.log("Home!!");
 
   headerSection.classList.remove("header-container--long");
-  headerSection.style.background = '';
-  arrowBtn.classList.add('inactive');
-  headerCategoryTitle.classList.add('inactive');
-  headerTitle.classList.add('inactive');
-  searchFormInput.classList.remove('inactive');
+  headerSection.style.background = "";
+  arrowBtn.classList.add("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.remove("inactive");
+  headerCategoryTitle.classList.add("inactive");
+  searchForm.classList.remove("inactive");
 
-  trendingPreviewSection.classList.remove('inactive');
-  categoriesPreviewSection.classList.remove('inactive');
-  genericSection.classList.add('inactive');
-  movieDetailSection.classList.add('inactive');
+  trendingPreviewSection.classList.remove("inactive");
+  categoriesPreviewSection.classList.remove("inactive");
+  genericSection.classList.add("inactive");
+  movieDetailSection.classList.add("inactive");
 
   getTrendingMoviesPreview();
   getCategegoriesPreview();
@@ -38,16 +51,68 @@ function homePage() {
 
 function categoriesPage() {
   console.log("categories!!");
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.add("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
 }
 
 function movieDetailsPage() {
   console.log("Movie!!");
+
+  headerSection.classList.add("header-container--long");
+  // headerSection.style.background = '';
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.add("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.add("inactive");
+  searchForm.classList.add("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.add("inactive");
+  movieDetailSection.classList.remove("inactive");
 }
 
 function searchPage() {
   console.log("Search!!");
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.remove("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
 }
 
 function trendsPage() {
   console.log("TRENDS!!");
-} 
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.add("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+}
